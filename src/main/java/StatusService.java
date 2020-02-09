c695539d98603999c744b3cbcc5c5f263b42fde3
AdrianIonutH <adriano_aih@yahoo.com>
2020-02-09 15:05:50 +0200

    Status table done

--- src/main/java/StatusService.java
+++ src/main/java/StatusService.java
@@ -1,3 +1,75 @@
-public class StatusService {
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
+public class StatusService {
+    private static StatusService instance;
+    private StatusService(){
+
+    }
+    public static StatusService getInstance(){
+        if (instance == null){
+            instance = new StatusService();
+        }
+        return instance;
+    }
+    public Status getStatus(Integer id) {
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            Status statusById = session.find(Status.class, id);
+            System.out.println(statusById);
+            return statusById;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
+    public void createStatus(int itemId, String createStatus){
+        Transaction transaction = null;
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            Comment comment = new Comment();
+            comment.itemId = itemId;
+            comment.text = createStatus;
+            session.save(comment);
+            transaction.commit();
+            session.close();
+        }catch (Exception ex){
+            if (transaction != null){
+                transaction.rollback();
+            }
+            ex.printStackTrace();
+        }
+    }
+    public void updateStatus(int id, String updateStatus) {
+        Transaction transaction = null;
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            Status statusById = getStatus(id);
+            statusById.statusName = updateStatus;
+            session.update(statusById);
+            session.getTransaction().commit();
+        } catch (Exception ex) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            ex.printStackTrace();
+        }
+    }
+    public void deleteStatus(int id) {
+        Transaction transaction = null;
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            Status statusById = getStatus(id);
+            session.delete(statusById);
+            session.getTransaction().commit();
+        } catch (Exception ex) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            ex.printStackTrace();
+        }
+    }
 }
